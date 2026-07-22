3a733e99312ed6c837e39ea09c092175ab67e28a
Javier Rodriguez <xjavierx1995@gmail.com>
2026-07-22 02:39:35 -0400

    FEAT: allow hot reaload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    host: "0.0.0.0",
+
+    port: 5173,
+
+    strictPort: true,
+
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+
+    hmr: {
+      host: "localhost",
+      port: 5173,
+    },
+
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
